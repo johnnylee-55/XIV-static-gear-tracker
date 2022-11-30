871c44e8d45fe35e246a6b515aeea9e741ab4846
johnnylee-55 <leejohnny5533@gmail.com>
2022-11-30 12:48:57 -0800

    added main method

---
+++ src/main/java/com/promineotech/tracker/GearTracker.java
@@ -0,0 +1,12 @@
+package com.promineotech.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearTracker {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GearTracker.class, args);
+  }
+}

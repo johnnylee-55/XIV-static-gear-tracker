ffa6964c1538814d636e6e0ec1fd47c86300c27e
johnnylee-55 <leejohnny5533@gmail.com>
2022-12-02 11:24:25 -0800

    added console logging, attempted to add openAPI documentation (not working)

--- src/main/java/com/promineotech/tracker/controller/BasicGearTrackerController.java
+++ src/main/java/com/promineotech/tracker/controller/BasicGearTrackerController.java
@@ -2,13 +2,15 @@ package com.promineotech.tracker.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import com.promineotech.tracker.entity.RaidingStatic;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class BasicGearTrackerController implements GearTrackerController {
 
   @Override
   public RaidingStatic fetchRaidingStatic(Integer raidingStaticId) {
-    
+    log.info("raidingStaticId={}", raidingStaticId);
     return null;
   }
 
